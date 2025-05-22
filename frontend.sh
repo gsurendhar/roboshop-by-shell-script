68ebd2b8e9ce245d593598089dbcdad9044728d0
gsurendhar <surendharg123@gmail.com>
2025-05-22 17:34:50 +0530

    frontend app file is added

--- frontend.sh
+++ frontend.sh
@@ -35,36 +35,36 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? " Disabling Default nginx "
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx:1.24 "
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx:1.24"
 
-systemctl enable nginx 
+systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx service"
 
-systemctl start nginx 
+systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing Default Content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading Frontend"
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping Frontend"
 
-rm -rf /etc/nginx/nginx.conf
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Removing Default nginx conf file"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Adding nginx conf file"
 
-systemctl restart nginx 
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting Nginx"
