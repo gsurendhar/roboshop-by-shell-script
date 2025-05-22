6403a8fdda331d2261e72cb0f945ee8f909623ba
gsurendhar <surendharg123@gmail.com>
2025-05-22 17:08:22 +0530

    catalogue idempotency file is added

---
+++ catalogue-idempotency.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#variables
+START_SCRIPT=$(date +%S)
+USERID=$(id -u)
+SCRIPT_DIR=$PWD
+LOG_FOLDER="/var/log/roboshop-shell-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+R="\e[31"
+G="\e[31"
+Y="\e[31"
+N="\e[31"
+
+echo "Script execution started at $START_SCRIPT " | tee -a $LOG_FILE
+
+mkdir -p $LOG_FOLDER
+
+# ROOT PRIVILEGES CHECKING
+if [ $USERID -ne 0 ]
+then 
+    echo -e " $R ERROR:$N Please run Script with the root access " | tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " You are already running with $Y ROOT $N access " | tee -a $LOG_FILE
+fi
+
+# VALIDATION FUNCTION
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ........$G SUCCESSES $N"  | tee -a $LOG_FILE
+    else    
+        echo -e "$2 is .........$R FAILURE $N"   | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling Default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enabling Default nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs:20 "
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "Roboshop system user " roboshop 
+    VALIDATE $? "Roboshop system user creating" 
+else
+    echo -e "roboshop user is already Created ....$Y SKIPPING USER Creation $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating App directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Downloading Catalogue"
+
+cd /app 
+rm -rf /app/*
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzipping Catalogue"
+
+npm install 
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "catalogue service file is copied "
+
+systemctl daemon-reload
+VALIDATE $? "Daemon Realoding"
+
+systemctl enable catalogue
+VALIDATE $? "Catalogue is enabling"
+
+systemctl start catalogue
+VALIDATE $? "Staring the Catalogue service"
+
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
+dnf install mongodb-mongosh -y
+VALIDATE $? "Installing Mongodb Client"
+
+STATUS=$(mongosh --host mongo.daws84s.site --eval ' db.getMongo().getDBNames.indexof(""catalogue")')
+if [ $STATUS -lt 0 ]
+then 
+    mongosh --host mongodb.daws84s.site < /app/db/master-data.js
+    VALIDATE $? "Loading the Data into MongoDB"
+else    
+    echo -e "Data is already loaded ....$Y SKIPPING $N "
+fi
+
+
+
+
+
+
+
+
