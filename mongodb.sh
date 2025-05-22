a71e6e4d0bd0e3cca06ed095eff0cb73b3ed7863
gsurendhar <surendharg123@gmail.com>
2025-05-22 16:00:22 +0530

    mongodb installtion file is added

---
+++ mongodb.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#variables
+START_SCRIPT=$(date +%S)
+USERID=$(id -u)
+SCRIPT_DIR=$PWD
+LOG_FOLDER="/var/log/roboshop-shell-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+R="\e[31"
+G="\e[31"
+Y="\e[31"
+N="\e[31"
+
+echo "Script execution started at $START_SCRIPT " | tee -a $LOG_FILE
+
+mkdir -p $LOG_FOLDER
+
+# ROOT PRIVILEGES CHECKING
+if [ $USERID -ne 0 ]
+then 
+    echo -e " $R ERROR:$N Please run Script with the root access " | tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " You are already running with $Y ROOT $N access " | tee -a $LOG_FILE
+fi
+
+# VALIDATION FUNCTION
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ........$G SUCCESSES $N"  | tee -a $LOG_FILE
+    else    
+        echo -e "$2 is .........$R FAILURE $N"   | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+VALIDATE $? "Copying mongodb repo"
+
+dnf install mongodb-org -y   &>>$LOG_FILE
+VALIDATE $? "MongoDB installation"
+
+systemctl enable mongod   &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB Service "
+
+systemctl start mongod    &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB Service "
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf     &>>$LOG_FILE
+VALIDATE $? "Editing mongodb conf file for remote connection "
+
+systemctl restart mongod    &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB Service "
