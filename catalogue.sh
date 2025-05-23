cb321e561ab9ee5162eef4e9c5cf60eccd09e2f2
gsurendhar <surendharg123@gmail.com>
2025-05-23 11:29:41 +0530

    catalogue app setup file updated

--- catalogue.sh
+++ catalogue.sh
@@ -76,6 +76,10 @@ cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb Client"
 
+mongosh --host mongodb.daws84s.site </app/db/master-data.js
+VALIDATE $? "Loading data to mongodb"
+
+
 
 
 
