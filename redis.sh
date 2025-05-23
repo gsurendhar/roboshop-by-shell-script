24948cb5508d58c0e8e5bb269e4b87f1245de88d
gsurendhar <surendharg123@gmail.com>
2025-05-23 11:49:05 +0530

    in Redis logs setup updated

--- redis.sh
+++ redis.sh
@@ -35,24 +35,24 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Redis "
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y  &>>$LOG_FILE
 VALIDATE $? "Enabling Redis:7 "
 
-dnf install redis -y 
+dnf install redis -y   &>>$LOG_FILE
 VALIDATE $? "Installing Redis:7 "
 
-sed -i -e '/s/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf
+sed -i -e '/s/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf  &>>$LOG_FILE
 VALIDATE $? "Edited Redis conf file to accept remote connections"
 
-systemctl enable redis 
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling Redis service"
 
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting the Redis service"
 
 END_TIME=$(date +%S)
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo -e "Script Execution Completed Successfully, $Y time taken : $TOTAL_TIME Seconds $N  "
+TOTAL_TIME=$(($END_TIME-$START_TIME)) &>>$LOG_FILE
+echo -e "Script Execution Completed Successfully, $Y time taken : $TOTAL_TIME Seconds $N" | tee -a $LOG_FILE
